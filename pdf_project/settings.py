0781faa1ca4ce2e700b23d2a4912a0de429c76c5
Rohit Patidar <patidar_rohit@yahoo.co.in>
2023-09-25 19:18:44 -0400

    updated yaml

--- pdf_project/settings.py
+++ pdf_project/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
