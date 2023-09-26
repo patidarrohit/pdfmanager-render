b06c983a4bca8448f36c2c7d1e27cea95ca5adbc
Rohit Patidar <patidar_rohit@yahoo.co.in>
2023-09-26 09:46:46 -0400

    add host

--- pdf_project/settings.py
+++ pdf_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 
 
-ALLOWED_HOSTS = ['pdfwala.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['pdfcolt.com', '127.0.0.1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
